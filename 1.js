cc36839ea7cd054bc482278b5c0504836ca14749
Ruslan8892 <hujop2021@gmail.com>
2025-01-17 19:30:32 +0200

    1

---
+++ 1.js
@@ -0,0 +1,7 @@
+let productCount = 4
+let productName = "Egg"
+
+productCount = 5;
+productName = "Milk";
+console.log(`Product Count: ${productCount}`);
+console.log(`Product Name: ${productName}`);
