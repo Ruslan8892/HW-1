d8201cfde1126532f623d9cfe396852476dfd224
Ruslan8892 <hujop2021@gmail.com>
2025-01-17 19:41:13 +0200

    2

---
+++ 2.js
@@ -0,0 +1,6 @@
+let UserSalary = 50000;
+let UserPosition = "Manager";
+UserSalary = 60000;
+UserPosition = "Senior Manager";
+console.log('UserSalary': ${UserSalary});
+console.log('UserPosition': ${UserPosition})
